6ac3641daa4350fff6fb1dba8bb0f46bc2c9e1e4
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-09-21 08:48:06 +0300

    completed import export exercise

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,12 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
-
+import { trees, elevation, wildlife} from "./parks/RockyMountain";
 function ColoradoStateParks() {
-  howManyParks(); // => "42 parks!"
+  howManyParks(); 
+  elevation()
+  wildlife()
+  console.log(trees)
 
   return <h1>Colorado State Parks!</h1>;
 }
+export default ColoradoStateParks;
